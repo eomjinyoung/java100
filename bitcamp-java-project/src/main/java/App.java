11c18825ad3f998c491d4bb012d04833a7789a02
eomjinyoung <jinyoung.eom@gmail.com>
2017-09-25 13:03:33 +0900

    .

--- bitcamp-java-project/src/main/java/App.java
+++ bitcamp-java-project/src/main/java/App.java
@@ -16,6 +16,17 @@
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.print("이름: ");
+        System.out.println("홍길동");
+        System.out.print("국어: ");
+        System.out.println(100);
+        System.out.print("영어: ");
+        System.out.println(100);
+        System.out.print("수학: ");
+        System.out.println(100);
+        System.out.print("총점: ");
+        System.out.println(300);
+        System.out.print("평균: ");
+        System.out.println(100.0);
     }
 }
