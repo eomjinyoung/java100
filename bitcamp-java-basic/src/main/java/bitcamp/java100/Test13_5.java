9c8607210c049ac1c5064bf759e10a8e7b2617a2
eomjinyoung <jinyoung.eom@gmail.com>
2017-09-28 12:00:29 +0900

    .

---
+++ bitcamp-java-basic/src/main/java/bitcamp/java100/Test13_5.java
@@ -0,0 +1,23 @@
+// ## 다양한 데이터 타입의 배열 
+// - int 외에 다른 데이터 타입의 배열을 만들어 보자!
+//
+
+package bitcamp.java100;
+
+public class Test13_5 {
+
+    public static void main(String[] args) {
+// java primitive data type 배열
+        int[] arr1 = {100, 100, 90};
+        byte[] arr2 = {10, 20, 30};
+        short[] arr3 = {12000, 22000, 25000};
+        long[] arr4 = {10L, 20L, 30L};
+        float[] arr5 = {3.14f, 2.45f, 345.94f};
+        double[] arr6 = {3.14, 2.45, 345.94};
+        boolean[] arr7 = {true, true, false, true};
+        char[] arr8 = {'홍', '길', '동'};
+
+// 문자열 배열
+        String[] names = {"홍길동", "임꺽정", "유관순"};
+    }
+}
