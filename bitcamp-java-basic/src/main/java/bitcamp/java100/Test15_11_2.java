700156d23796df36fde354d1c68dd50f98a9f179
eomjinyoung <jinyoung.eom@gmail.com>
2017-10-11 12:20:03 +0900

    .

--- bitcamp-java-basic/src/main/java/bitcamp/java100/Test15_11_2.java
+++ bitcamp-java-basic/src/main/java/bitcamp/java100/Test15_11_2.java
@@ -10,47 +10,34 @@ public class Test15_11_2 {
 // 임무: 파일의 정보를 저장하라!
 // - 디렉토리여부, 소유주/그룹/기타 사용자의 읽기, 쓰기, 실행 권한 정보
 //
-// 방법1: 클래스를 이용하여 처리        
-        // => 메모리 설계
-        class Privilege {
-            boolean isDirectory;
-            boolean ownerRead;
-            boolean ownerWrite;
-            boolean ownerExe;
-            boolean groupRead;
-            boolean groupWrite;
-            boolean groupExe;
-            boolean otherRead;
-            boolean otherWrite;
-            boolean otherExe;
-        }
+// 방법2: 배열 이용하여 처리        
         
-        // => Privilege 설계도에 따라 메모리를 준비하고 그 메모리의 주소를 저장한다.
-        Privilege p1 = new Privilege();
+        // => 사용자 권한 정보를 담을 boolean 메모리를 여러 개 만든 후 그 주소를 보관한다.
+        boolean[] arr = new boolean[10];
         
-        // => 메모리 주소(레퍼런스)를 가지고 해당 메모리의 각 항목에 값을 넣는다.
-        p1.isDirectory = false;
-        p1.ownerRead = true;
-        p1.ownerWrite = true;
-        p1.ownerExe = true;
-        p1.groupRead = true;
-        p1.groupWrite = false;
-        p1.groupExe = true;
-        p1.otherRead = true;
-        p1.otherWrite = false;
-        p1.otherExe = true;
+        // => 배열에 사용자 권한을 저장한다.
+        arr[0] = false;
+        arr[1] = true;
+        arr[2] = true;
+        arr[3] = true;
+        arr[4] = true;
+        arr[5] = false;
+        arr[6] = true;
+        arr[7] = true;
+        arr[8] = false;
+        arr[9] = true;
         
         // => 사용 권한 검사
-        System.out.print(p1.isDirectory ? "d" : "-");
-        System.out.print(p1.ownerRead ? "r" : "-");
-        System.out.print(p1.ownerWrite ? "w" : "-");
-        System.out.print(p1.ownerExe ? "x" : "-");
-        System.out.print(p1.groupRead ? "r" : "-");
-        System.out.print(p1.groupWrite ? "w" : "-");
-        System.out.print(p1.groupExe ? "x" : "-");
-        System.out.print(p1.otherRead ? "r" : "-");
-        System.out.print(p1.otherWrite ? "w" : "-");
-        System.out.print(p1.otherExe ? "x" : "-");
+        System.out.print(arr[0] ? "d" : "-");
+        System.out.print(arr[1] ? "r" : "-");
+        System.out.print(arr[2] ? "w" : "-");
+        System.out.print(arr[3] ? "x" : "-");
+        System.out.print(arr[4] ? "r" : "-");
+        System.out.print(arr[5] ? "w" : "-");
+        System.out.print(arr[6] ? "x" : "-");
+        System.out.print(arr[7] ? "r" : "-");
+        System.out.print(arr[8] ? "w" : "-");
+        System.out.print(arr[9] ? "x" : "-");
         System.out.println();
         
     }
