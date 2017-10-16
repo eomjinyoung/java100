38dd1bbc52f4d685ab71ae0f5b9aa0a60031d0ab
eomjinyoung <jinyoung.eom@gmail.com>
2017-10-16 10:40:19 +0900

    .

---
+++ bitcamp-java-basic/examples/ch03/Test17_14.java
@@ -0,0 +1,66 @@
+// ## 메서드 - call by value 와 call by reference
+// - call by value와 call by reference의 차이점을 알아보자!
+//
+
+package bitcamp.java100;
+
+public class Test17_14 {
+
+    // ### call by value
+    // - 메서드를 호출할 때 값을 넘긴다.
+    // - 호출자의 로컬 변수의 값을 변경할 수 없다.
+    static void swap(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.printf("swap(): a = %d, b = %d\n", a, b);
+    }
+    
+    // ### call by reference
+    // - 메서드를 호출할 때 메모리의 주소를 넘긴다.
+    // - 메서드에서 호출자의 메모리 값을 변경할 수 있다.
+    static void swap2(int[] r) {
+        int temp = r[0];
+        r[0] = r[1];
+        r[1] = temp;
+        System.out.printf("swap2(): r[0] = %d, r[1] = %d\n", r[0], r[1]);
+    }
+    
+    public static void main(String[] args) {
+        // 호출할 때 값을 넘기는 예:
+        // => "call by value"라 부른다.
+        int a = 5;
+        int b = 7;
+        
+        swap(a, b); 
+        
+        System.out.printf("main(): a = %d, b = %d\n", a, b);
+        
+        
+        // 호출할 때 메모리 주소를 넘기는 예:
+        // => "call by reference"라 부른다.
+        int[] arr = new int[2];
+        arr[0] = 5;
+        arr[1] = 7;
+        
+        swap2(arr);
+        
+        System.out.printf("main(): arr[0] = %d, arr[1] = %d\n", arr[0], arr[1]);
+        
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ bitcamp-java-basic/examples/ch03/Test17_9.java
@@ -0,0 +1,75 @@
+// ## 메서드 - 메서드 정의 문법 VIII
+// - 메서드 정의 기본 문법을 알아 보자!
+//
+
+package bitcamp.java100;
+
+public class Test17_9 {
+
+    // 8) 값을 받아서 계산한 후 그 결과를 리턴하는 메서드
+    
+    // 사용자 정의 데이터 타입 만들기
+    static class Score {
+        String name;
+        int[] subjects = new int[3];
+        int sum;
+        float aver;
+    }
+    
+    // 
+    static Score createMember(String name, int kor, int eng, int math) {
+        Score s = new Score();
+        s.name = name;
+        s.subjects[0] = kor;
+        s.subjects[1] = eng;
+        s.subjects[2] = math;
+        s.sum = kor + eng + math;
+        s.aver = s.sum / 3f;
+        
+        return s; // 절대 잊지 말자! 리턴하는 것은 객체의 주소이다.
+    }
+    
+    // int 값 두 개 받아서 계산을 한 후 int 값 한 개를 리턴하는 메서드
+    static int exp(int value, int ex) {
+        int sum = 1;
+        for (int i = 0; i < ex; i++) {
+            sum *= value;
+        }
+        return sum;
+    }
+    
+    public static void main(String[] args) {
+        // createMember()가 리턴하는 것은 객체 주소이다.
+        // 그 주소를 레퍼런스 score 변수에 저장한다.
+        Score score = createMember("홍길동", 100, 90, 80);
+        
+        // score 변수에 저장된 주소로 찾아가서 그 메모리의 각 항목 값을 꺼낸다.
+        System.out.println(score.name);
+        System.out.println(score.subjects[0]);
+        System.out.println(score.subjects[1]);
+        System.out.println(score.subjects[2]);
+        System.out.println(score.sum);
+        System.out.println(score.aver);
+        
+        System.out.println("------------------------");
+        
+        // 2의 8승을 구해보자!
+        int result = exp(2, 8);
+        System.out.println(result);
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
