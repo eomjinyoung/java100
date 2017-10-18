864e00de211e4d1d11fd1bb03378f0e4bc8d30ab
eomjinyoung <jinyoung.eom@gmail.com>
2017-10-18 16:17:58 +0900

    .

---
+++ bitcamp-java-basic/src/main/java/bitcamp/java100/Test21_6.java
@@ -0,0 +1,44 @@
+// ## 키보드로부터 입력 받기 - 연습2
+// - 다음과 같이 사용자로부터 숫자를 입력 받아 각 숫자의 개수를 세시오.
+// - 실행 예) 
+// 숫자? 23347651
+// 0 = 0
+// 1 = 1
+// 2 = 1
+// 3 = 2
+// 4 = 1
+// 5 = 1
+// 6 = 1
+// 7 = 1
+// 8 = 0
+// 9 = 0
+// > 
+
+package bitcamp.java100;
+
+import java.io.Console;
+
+public class Test21_6 {
+
+    public static void main(String[] args) {
+        // 콘솔 객체를 준비한다.
+        Console console = System.console();
+        
+        if (console == null) {
+            System.err.println("콘솔을 지원하지 않습니다.");
+            System.exit(1); // JVM을 종료한다.
+        }
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ bitcamp-java-basic/src/main/java/bitcamp/java100/Test21_7.java
@@ -0,0 +1,36 @@
+// ## 키보드로부터 입력 받기 - 연습3
+// - 다음과 같이 사용자로부터 문자(숫자와 알파벳)를 입력 받아 꺼꾸로 변경하시오!
+// - 실행 예) 
+// 문자열? AB2CTT980
+// 입력한 값: AB2CTT980
+// 꺼꾸로 바꾼 값: 089TTC2BA
+// > 
+
+package bitcamp.java100;
+
+import java.io.Console;
+
+public class Test21_7 {
+
+    public static void main(String[] args) {
+        // 콘솔 객체를 준비한다.
+        Console console = System.console();
+        
+        if (console == null) {
+            System.err.println("콘솔을 지원하지 않습니다.");
+            System.exit(1); // JVM을 종료한다.
+        }
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
