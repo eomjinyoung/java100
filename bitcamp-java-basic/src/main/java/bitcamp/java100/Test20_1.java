2e5762cc4ce4ebfd0f22d7c85ad05a5f84ca7491
eomjinyoung <jinyoung.eom@gmail.com>
2017-10-18 12:22:31 +0900

    .

---
+++ bitcamp-java-basic/src/main/java/bitcamp/java100/Test20_1.java
@@ -0,0 +1,66 @@
+// ## 래퍼(wrapper) 클래스 사용법 - 래퍼 클래스의 소개
+// - 래퍼 클래스가 무엇인지, 어떤 경우에 사용하는지 알아보자!
+//
+
+package bitcamp.java100;
+
+public class Test20_1 {
+    public static void main(String[] args) {
+        // primitive type 변수
+        int i = 10;
+        
+        // int 래퍼 클래스 사용
+        Integer i2 = new Integer(10);
+        
+        // 래퍼(wrapper) 클래스?
+        // - primitive 값을 객체로 다루기 위해 제공되는 클래스이다.
+        // - 각각의 primitive 타입에 대한 클래스가 있다.
+        //   byte       => java.lang.Byte
+        //   short      => java.lang.Short
+        //   int        => java.lang.Integer
+        //   long       => java.lang.Long
+        //   float      => java.lang.Float
+        //   double     => java.lang.Double
+        //   boolean    => java.lang.Boolean
+        //   char       => java.lang.Character
+        //
+        // primitive 값을 객체로 다루는 이유?
+        // - 클래스에서 제공하는 다양한 메서드를 이용할 수 있다.
+        //   좀 더 정교하게 값을 다룰 수 있다.
+        // - 값이나 객체의 주소를 구분하지 않고 주고 받고 싶을 때 사용한다. 
+        
+        // intValue()
+        // > 객체에 저장된 int 값을 뽑아서 리턴해준다.
+        int i3 = i2.intValue();
+        
+        // floatValue()
+        // > 객체에 저장된 int 값을 float으로 만들어 리턴한다.
+        float f1 = i2.floatValue();
+        
+        // Integer 클래스에는 int로 저장할 수 있는 최소/최대 값을 알려주는 변수가 있다.
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        
+        // Integer 클래스에는 문자열을 해석하여 int로 변환해주는 메서드도 있다.
+        int value = Integer.parseInt("2345");
+        System.out.println(value + 100);
+        
+        // 또한 int 값을 2진수, 8진수, 16진수로 표현된 문자열로 바꾸는 메서드도 있다.
+        System.out.println(Integer.toBinaryString(44032));
+        System.out.println(Integer.toOctalString(44032));
+        System.out.println(Integer.toHexString(44032));
+        
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ bitcamp-java-basic/src/main/java/bitcamp/java100/Test20_2.java
@@ -0,0 +1,41 @@
+// ## 래퍼(wrapper) 클래스 사용법 - 오토박싱 & 언박싱
+// - 오토 박싱/ 오토 언박싱의 개념과 사용법을 알아보자!
+//
+
+package bitcamp.java100;
+
+public class Test20_2 {
+    public static void main(String[] args) {
+        int i = 100;
+        
+        // 오토 박싱(auto-boxing)
+        // - primitive 값을 레퍼런스 변수에 저장하려 할 때 
+        //   자동으로 래퍼 클래스의 인스턴스를 생성한 후 그 주소를 리턴한다.
+        // - 이렇게 값이 객체에 넣어져서 리턴된다고 해서 "상자에 담는다(boxing)"라는
+        //   표현을 사용한다.
+        // 
+        Integer r1 = 100; // OK! => new Integer(100)
+        
+        Integer r2 = new Integer(300);
+        
+        // 오토 언박싱(auto-unboxing)
+        // - 랩퍼 객체를 primitive 타입의 변수에 저장하려 할 때 
+        //   자동으로 래퍼 객체의 값을 꺼내는 것을 말한다.
+        // - 즉 객체에 들어 있는 값을 꺼낸다고 해서 "상자에서 꺼낸다(unboxing)"라고 
+        //   부른다.
+        // 
+        int i2 = r2; // OK! => r2.intValue()
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
