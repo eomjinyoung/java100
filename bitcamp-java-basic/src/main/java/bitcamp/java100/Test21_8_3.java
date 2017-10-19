21d2328d86035bec2d2d6530ef23b9e67ca0061b
eomjinyoung <jinyoung.eom@gmail.com>
2017-10-19 16:55:08 +0900

    .

--- bitcamp-java-basic/src/main/java/bitcamp/java100/Test21_8_3.java
+++ bitcamp-java-basic/src/main/java/bitcamp/java100/Test21_8_3.java
@@ -35,15 +35,8 @@ package bitcamp.java100;
 
 import java.io.Console;
 
-// 5단계: 연락처 데이터를 저장할 새로운 데이터 타입을 만든다.
-public class Test21_8 {
-    
-    // 새 데이터 타입 정의
-    static class Contact {
-        String name;
-        String email;
-        String tel;
-    }
+// 3단계: 계속 입력할지 여부를 묻는 기능 추가
+public class Test21_8_3 {
 
     public static void main(String[] args) {
         // 콘솔 객체를 준비한다.
@@ -54,32 +47,21 @@ public class Test21_8 {
             System.exit(1); // JVM을 종료한다.
         }
         
-        // Contact 값을 저장할 레퍼런스 배열을 만든다.
-        Contact[] contacts = new Contact[100];
+        // 값을 저장할 레퍼런스 배열을 만든다.
+        String[] names = new String[100];
+        String[] emails = new String[100];
+        String[] tels = new String[100];
         
         // 값이 저장될 위치
         int cursor = 0;
         
         // 배열의 개수 만큼 입력을 받는다.
-        while (cursor < contacts.length) {
-            // 사용자가 입력한 데이터를 저장할 메모리 준비
-            Contact contact = new Contact();
-            
-            // 사용자가 입력한 데이터를 Contact 인스턴스(객체)에 저장한다.
-            contact.name = console.readLine("이름? ");
-            contact.email = console.readLine("이메일? ");
-            contact.tel = console.readLine("전화? ");
-            
-            String response = console.readLine("저장하시겠습니까?(y/n) ");
-            
-            if (response.toLowerCase().equals("y") || 
-                    response.toLowerCase().equals("yes")) {
-                // 이름, 이메일, 전화 데이터가 들어있는 인스턴스 주소를 배열에 저장.
-                contacts[cursor] = contact;
-                cursor++;
-            }
-            
-            response = console.readLine("계속 입력하시겠습까?(y/n) ");
+        while (cursor < names.length) {
+            names[cursor] = console.readLine("이름? ");
+            emails[cursor] = console.readLine("이메일? ");
+            tels[cursor] = console.readLine("전화? ");
+            cursor++;
+            String response = console.readLine("계속 입력하시겠습까?(y/n) ");
             if (!(response.toLowerCase().equals("y") || 
                   response.toLowerCase().equals("yes")))
                 break;
@@ -87,10 +69,9 @@ public class Test21_8 {
         
         // 배열에 저장된 값을 출력한다.
         for (int i = 0; i < cursor; i++) {
-            System.out.printf("%s, %s, %s\n", 
-                    contacts[i].name, 
-                    contacts[i].email, 
-                    contacts[i].tel);
+            System.out.println(names[i]);
+            System.out.println(emails[i]);
+            System.out.println(tels[i]);
         }
         
     }
--- bitcamp-java-basic/src/main/java/bitcamp/java100/Test21_8_4.java
+++ bitcamp-java-basic/src/main/java/bitcamp/java100/Test21_8_4.java
@@ -35,15 +35,8 @@ package bitcamp.java100;
 
 import java.io.Console;
 
-// 5단계: 연락처 데이터를 저장할 새로운 데이터 타입을 만든다.
-public class Test21_8 {
-    
-    // 새 데이터 타입 정의
-    static class Contact {
-        String name;
-        String email;
-        String tel;
-    }
+// 4단계: 저장할지 말지 여부를 묻는 기능을 추가한다.
+public class Test21_8_4 {
 
     public static void main(String[] args) {
         // 콘솔 객체를 준비한다.
@@ -54,28 +47,27 @@ public class Test21_8 {
             System.exit(1); // JVM을 종료한다.
         }
         
-        // Contact 값을 저장할 레퍼런스 배열을 만든다.
-        Contact[] contacts = new Contact[100];
+        // 값을 저장할 레퍼런스 배열을 만든다.
+        String[] names = new String[100];
+        String[] emails = new String[100];
+        String[] tels = new String[100];
         
         // 값이 저장될 위치
         int cursor = 0;
         
         // 배열의 개수 만큼 입력을 받는다.
-        while (cursor < contacts.length) {
-            // 사용자가 입력한 데이터를 저장할 메모리 준비
-            Contact contact = new Contact();
-            
-            // 사용자가 입력한 데이터를 Contact 인스턴스(객체)에 저장한다.
-            contact.name = console.readLine("이름? ");
-            contact.email = console.readLine("이메일? ");
-            contact.tel = console.readLine("전화? ");
+        while (cursor < names.length) {
+            String name = console.readLine("이름? ");
+            String email = console.readLine("이메일? ");
+            String tel = console.readLine("전화? ");
             
             String response = console.readLine("저장하시겠습니까?(y/n) ");
             
             if (response.toLowerCase().equals("y") || 
                     response.toLowerCase().equals("yes")) {
-                // 이름, 이메일, 전화 데이터가 들어있는 인스턴스 주소를 배열에 저장.
-                contacts[cursor] = contact;
+                names[cursor] = name;
+                emails[cursor] = email;
+                tels[cursor] = tel;
                 cursor++;
             }
             
@@ -87,10 +79,7 @@ public class Test21_8 {
         
         // 배열에 저장된 값을 출력한다.
         for (int i = 0; i < cursor; i++) {
-            System.out.printf("%s, %s, %s\n", 
-                    contacts[i].name, 
-                    contacts[i].email, 
-                    contacts[i].tel);
+            System.out.printf("%s, %s, %s\n", names[i], emails[i], tels[i]);
         }
         
     }
