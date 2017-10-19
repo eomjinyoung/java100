c713a832428e72dd18dc57dfbf26cd86f1228c58
eomjinyoung <jinyoung.eom@gmail.com>
2017-10-19 11:57:38 +0900

    .

--- bitcamp-java-basic/src/main/java/bitcamp/java100/Test21_6_4.java
+++ bitcamp-java-basic/src/main/java/bitcamp/java100/Test21_6_4.java
@@ -18,31 +18,45 @@ package bitcamp.java100;
 
 import java.io.Console;
 
-// 1단계: 문제해결 두 번째 방법 - 입력 문자열을 그대로 이용하기
-public class Test21_6 {
+// 2단계: 문제해결 두 번째 방법 - 메서드를 도입하여 코드 분리하기
+public class Test21_6_4 {
+    static Console console = System.console();
     
-    public static void main(String[] args) {
-        
-        // 콘솔 객체를 준비한다.
-        Console console = System.console();
-        
+    static void prepareConsole() {
+        console = System.console();
+
         if (console == null) {
             System.err.println("콘솔을 지원하지 않습니다.");
-            System.exit(1); // JVM을 종료한다.
+            System.exit(1); 
         }
-        
+    }
+    
+    static StringBuffer inputNumber() {
         StringBuffer buf = new StringBuffer(console.readLine("숫자? "));
-        
+        return buf;
+    }
+    
+    static int[] countNumber(StringBuffer buf) {
         int[] cnt = new int[10];
-        
+
         for (int i = 0; i < buf.length(); i++) {
             cnt[buf.charAt(i) - '0']++;
         }
-        
+        return cnt;
+    }
+    
+    static void printNumberCount(int[] cnt) {
         for (int i = 0; i < cnt.length; i++) {
             System.out.printf("%d = %d\n", i, cnt[i]);
         }
-        
+    }
+
+    public static void main(String[] args) {
+        prepareConsole();
+        StringBuffer buf = inputNumber();
+        int[] cnt = countNumber(buf);
+        printNumberCount(cnt);
+
     }
 }
 
