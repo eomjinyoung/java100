40a2a8b73d98354628289bd28197a84ddfe35ade
eomjinyoung <jinyoung.eom@gmail.com>
2017-10-19 12:56:50 +0900

    .

--- bitcamp-java-basic/src/main/java/bitcamp/java100/Test21_7.java
+++ bitcamp-java-basic/src/main/java/bitcamp/java100/Test21_7.java
@@ -21,7 +21,24 @@ public class Test21_7 {
             System.exit(1); // JVM을 종료한다.
         }
         
+        String str = console.readLine("문자열? ");
+        
+        StringBuffer buf = new StringBuffer();
+        buf.append(str);
+        
+        int len = buf.length() / 2;
+        
+        for (int left = 0, right = buf.length() - 1; left < len; left++, right--) {
+            char ch = buf.charAt(left);
+            buf.setCharAt(left, buf.charAt(right));
+            buf.setCharAt(right, ch);
+        }
+        
+        System.out.printf("입력 문자열: %s\n", str);
+        System.out.printf("변경 문자열: %s\n", buf);
     }
+    
+   
 }
 
 
