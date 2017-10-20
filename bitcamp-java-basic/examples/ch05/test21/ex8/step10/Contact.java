4d34ba3414d63b379ebff88ad757c3f35de22f16
eomjinyoung <jinyoung.eom@gmail.com>
2017-10-20 11:38:51 +0900

    .

---
+++ bitcamp-java-basic/examples/ch05/test21/ex8/step10/Contact.java
@@ -0,0 +1,8 @@
+package bitcamp.java100.test21.ex8.step10;
+
+//새 데이터 타입 정의
+public class Contact {
+    String name;
+    String email;
+    String tel;
+}
--- bitcamp-java-basic/src/main/java/bitcamp/java100/Test17_17/step4/Rect.java
+++
@@ -1,6 +0,0 @@
-package bitcamp.java100.Test17_17.step4;
-
-public class Rect {
-    int width;
-    int height;
-}
