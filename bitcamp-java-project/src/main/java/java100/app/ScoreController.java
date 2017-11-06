b08f6a4c09eb7704aacffff76ee4897ad6a7c010
eomjinyoung <jinyoung.eom@gmail.com>
2017-11-06 09:30:00 +0900

    .

--- bitcamp-java-project/src/main/java/java100/app/ScoreController.java
+++ bitcamp-java-project/src/main/java/java100/app/ScoreController.java
@@ -8,21 +8,6 @@ public class ScoreController {
     
     static Scanner keyScan = new Scanner(System.in);
     
-    // ArrayList 객체와 메서드들을 스태틱으로 둘 것인지,
-    // 아니면 인스턴스로 변경할 것인지 결정해야 한다.
-    // => 만약 프로그램에서 성적 목록을 동시에 다룰 일이 없다면
-    //    그냥 스태틱 멤버로 둔다.
-    // => 프로그램에서 성적 목록을 여러 개 생성할 일이 있다면
-    //    인스턴스 멤버로 선언해야 한다.
-    //    즉 개별적으로 다룰 데이터라면 인스턴스 멤버로 만들라!
-    // => 실무!
-    //    실무에서는 나중에 여러 개 사용할 것을 대비해 
-    //    보통 인스턴스 멤버로 선언한다.
-    //    물론, Prompts 클래스의 메서드처럼 인스턴스 변수를 
-    //    사용할 일이 확실히 없는 경우에는 스태틱으로 선언한다.
-    // 
-    
-    // 성적 데이터를 보관하는 목록 객체는 외부 노출을 막는다.
     private ArrayList<Score> list = new ArrayList<>();
     
     public void execute() {
@@ -45,14 +30,6 @@ public class ScoreController {
         }
     }
     
-    // 이전에는 App 클래스에서 doXxx() 메서드를 호출하기 때문에
-    // 공개하였다.
-    // 지금은 App 클래스에서 직접 doXxx()를 호출하는 게 아니라,
-    // 같은 멤버인 execute()에서 호출하기 때문에 공개할 필요가 없다.
-    // => 객체지향 프로그래밍에서는 가능한 공개 범위를 줄이는 것이
-    //    유지보수에 좋다.
-    //    즉 공개할 필요가 있을 때만 공개하라!
-    // 
     private void doDelete() {
         System.out.println("[성적 삭제]");
         String name = Prompts.input("이름? ");
@@ -145,4 +122,28 @@ public class ScoreController {
         }
         
     }
+    
+    private Score findByName(String name) {
+        Iterator<Score> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            Score score = iterator.next();
+            if (score.name.equals(name)) {
+                return score;
+            }
+        }
+        return null;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
