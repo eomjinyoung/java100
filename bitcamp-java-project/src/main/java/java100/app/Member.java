db1470e43f27e675ecde19bcda1c6c821b670874
eomjinyoung <jinyoung.eom@gmail.com>
2017-11-06 11:48:26 +0900

    .

--- bitcamp-java-project/src/main/java/java100/app/Member.java
+++ bitcamp-java-project/src/main/java/java100/app/Member.java
@@ -1,7 +1,5 @@
 package java100.app;
 
-import java.util.Scanner;
-
 public class Member {
     
     protected String name;
@@ -45,56 +43,6 @@ public class Member {
         this.password = password;
     }
     
-    public void print() {
-        System.out.printf("%-4s, %s\n",  
-                this.name, 
-                this.email);
-    }
-    
-    public void input() {
-        Scanner keyScan = new Scanner(System.in);
-        
-        System.out.print("이름? ");
-        this.name = keyScan.nextLine();
-        
-        System.out.print("이메일? ");
-        this.email = keyScan.nextLine();
-        
-        System.out.print("암호? ");
-        this.password = keyScan.nextLine();
-    }
-    
-    public void printDetail() {
-        System.out.printf("이름: %s\n", this.name);
-        System.out.printf("이메일: %s\n", this.email);
-        System.out.printf("암호: %s\n", this.password);
-        
-    }
-    
-    public void update() {
-        Scanner keyScan = new Scanner(System.in);
-        
-        System.out.printf("이름?(%s) ", this.name);
-        String name = keyScan.nextLine();
-        if (name.isEmpty()) {
-            name = this.name;
-        }
-        
-        System.out.printf("암호? ", this.password);
-        String password = keyScan.nextLine();
-        if (password.isEmpty()) {
-            password = this.password;
-        }
-        
-        if (Prompts.confirm2("변경하시겠습니까?(y/N) ")) {
-            this.name = name;
-            this.password = password;
-            System.out.println("변경하였습니다.");
-            
-        } else {
-            System.out.println("변경을 취소하였습니다.");
-        }
-    }    
 }
 
 
--- bitcamp-java-project/src/main/java/java100/app/MemberController.java
+++ bitcamp-java-project/src/main/java/java100/app/MemberController.java
@@ -35,66 +35,93 @@ public class MemberController {
         
         Iterator<Member> iterator = list.iterator();
         while (iterator.hasNext()) {
-            iterator.next().print();
+            Member member = iterator.next();
+            System.out.printf("%-4s, %s\n",  
+                    member.getName(), 
+                    member.getEmail());
         }
     }
     
     private void doAdd() {
         System.out.println("[회원 등록]");
         
-        Member member;
-        member = new Member(); 
-        member.input();
+        Member member = new Member();
+        member.setEmail(Prompts.inputString("이메일? "));
         
         if (findByEmail(member.getEmail()) != null) {
             System.out.println("이미 등록된 이메일입니다.");
-        } else {
-            list.add(member);
+            return;
         }
         
+        member.setName(Prompts.inputString("이름? "));
+        member.setPassword(Prompts.inputString("암호? "));
+        
+        list.add(member);
     } 
     
     private void doView() {
         System.out.println("[회원 상세 정보]");
-        String email = Prompts.input("이메일? ");
+        String email = Prompts.inputString("이메일? ");
         
         Member member = findByEmail(email);
         
         if (member == null) {
             System.out.printf("'%s'의 회원 정보가 없습니다.\n", email);
-        } else {
-            member.printDetail();
+            return;
         }
+        
+        System.out.printf("이름: %s\n", member.getName());
+        System.out.printf("이메일: %s\n", member.getEmail());
+        System.out.printf("암호: %s\n", member.getPassword());
     } 
     
     private void doUpdate() {
         System.out.println("[회원 변경]");
-        String email = Prompts.input("이메일? ");
+        String email = Prompts.inputString("이메일? ");
         
         Member member = findByEmail(email);
         
         if (member == null) {
             System.out.printf("'%s'의 회원 정보가 없습니다.\n", email);
+            return;
+        }
+        
+        String name = Prompts.inputString("이름?(%s) ", member.getName());
+        if (name.isEmpty()) {
+            name = member.getName();
+        }
+        
+        String password = Prompts.inputString("암호? ");
+        if (password.isEmpty()) {
+            password = member.getPassword();
+        }
+        
+        if (Prompts.confirm2("변경하시겠습니까?(y/N) ")) {
+            member.setName(name);
+            member.setPassword(password);
+            System.out.println("변경하였습니다.");
+            
         } else {
-            member.update();
+            System.out.println("변경을 취소하였습니다.");
         }
     }
     
     private void doDelete() {
         System.out.println("[회원 삭제]");
-        String email = Prompts.input("이메일? ");
+        String email = Prompts.inputString("이메일? ");
         
         Member member = findByEmail(email);
         
         if (member == null) {
             System.out.printf("'%s'의 회원 정보가 없습니다.\n", email);
+            return;
+        }
+        
+        if (Prompts.confirm2("정말 삭제하시겠습니까?(y/N) ")) {
+            list.remove(member);
+            System.out.println("삭제하였습니다.");
         } else {
-            if (Prompts.confirm2("정말 삭제하시겠습니까?(y/N) ")) {
-                list.remove(member);
-                System.out.println("삭제하였습니다.");
-            } else {
-                System.out.println("삭제를 취소하였습니다.");
-            }
+            System.out.println("삭제를 취소하였습니다.");
         }
     }
     
