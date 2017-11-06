59c0ccdab8a6a92221cc396ea75470af752851fe
eomjinyoung <jinyoung.eom@gmail.com>
2017-11-06 12:18:39 +0900

    .

--- bitcamp-java-project/src/main/java/java100/app/Board.java
+++ bitcamp-java-project/src/main/java/java100/app/Board.java
@@ -1,7 +1,6 @@
 package java100.app;
 
 import java.sql.Date;
-import java.util.Scanner;
 
 public class Board {
     protected int no;
@@ -58,59 +57,6 @@ public class Board {
         this.viewCount = viewCount;
     }
     
-    public void print() {
-        System.out.printf("%d, %s, %s, %d\n",  
-                this.no, 
-                this.title,
-                this.regDate.toString(),
-                this.viewCount);
-    }
-    
-    public void input() {
-        Scanner keyScan = new Scanner(System.in);
-        
-        System.out.print("번호? ");
-        this.no = Integer.parseInt(keyScan.nextLine());
-        
-        System.out.print("제목? ");
-        this.title = keyScan.nextLine();
-        
-        System.out.print("내용? ");
-        this.content = keyScan.nextLine();
-        
-        this.regDate = new Date(System.currentTimeMillis());
-    }
-    
-    public void printDetail() {
-        System.out.printf("제목: %s\n", this.title);
-        System.out.printf("내용: %s\n", this.content);
-        System.out.printf("등록일: %s\n", this.regDate.toString());
-        System.out.printf("조회수: %d\n", ++this.viewCount);
-        
-    }
-    
-    public void update() {
-        Scanner keyScan = new Scanner(System.in);
-        
-        System.out.printf("제목?(%s) ", this.title);
-        String title = keyScan.nextLine();
-        if (title.isEmpty()) {
-            title = this.title;
-        }
-        
-        System.out.printf("내용? ", this.content);
-        String content = keyScan.nextLine();
-        
-        if (Prompts.confirm2("변경하시겠습니까?(y/N) ")) {
-            this.title = title;
-            this.content = content;
-            this.regDate = new Date(System.currentTimeMillis());
-            System.out.println("변경하였습니다.");
-            
-        } else {
-            System.out.println("변경을 취소하였습니다.");
-        }
-    }
 }
 
 
--- bitcamp-java-project/src/main/java/java100/app/BoardController.java
+++ bitcamp-java-project/src/main/java/java100/app/BoardController.java
@@ -1,5 +1,6 @@
 package java100.app;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
@@ -34,7 +35,12 @@ public class BoardController {
         
         Iterator<Board> iterator = list.iterator();
         while (iterator.hasNext()) {
-            iterator.next().print();
+            Board board = iterator.next();
+            System.out.printf("%d, %s, %s, %d\n",  
+                    board.getNo(), 
+                    board.getTitle(),
+                    board.getRegDate().toString(),
+                    board.getViewCount());
         }
     }
 
@@ -42,57 +48,83 @@ public class BoardController {
         System.out.println("[게시물 등록]");
         
         Board board = new Board(); 
-        board.input();
-        
+        board.setNo(Prompts.inputInt("번호? "));
+
         if (findByNo(board.getNo()) != null) {
             System.out.println("이미 등록된 번호입니다.");
-        } else {
-            list.add(board);
-        }
+            return;
+        } 
+            
+        board.setTitle(Prompts.inputString("제목? "));
+        board.setContent(Prompts.inputString("내용? "));
+        board.setRegDate(new Date(System.currentTimeMillis()));
         
+        list.add(board);
     } 
     
     private void doView() {
         System.out.println("[게시물 상세 정보]");
-        int no = Integer.parseInt(Prompts.input("번호? "));
+        int no = Prompts.inputInt("번호? ");
         
         Board board = findByNo(no);
         
         if (board == null) {
             System.out.printf("%d번 게시물이 없습니다.\n", no);
-        } else {
-            board.printDetail();
+            return;
         }
+        
+        System.out.printf("제목: %s\n", board.getTitle());
+        System.out.printf("내용: %s\n", board.getContent());
+        System.out.printf("등록일: %s\n", board.getRegDate().toString());
+        board.setViewCount(board.getViewCount() + 1);
+        System.out.printf("조회수: %d\n", board.getViewCount());
     } 
     
     private void doUpdate() {
         System.out.println("[게시물 변경]");
-        int no = Integer.parseInt(Prompts.input("번호? "));
+        int no = Prompts.inputInt("번호? ");
         
         Board board = findByNo(no);
         
         if (board == null) {
             System.out.printf("%d번 게시물이 없습니다.\n", no);
+            return;
+        } 
+        
+        String title = Prompts.inputString("제목?(%s) ", board.getTitle());
+        if (title.isEmpty()) {
+            title = board.getTitle();
+        }
+        
+        String content = Prompts.inputString("내용? ");
+        
+        if (Prompts.confirm2("변경하시겠습니까?(y/N) ")) {
+            board.setTitle(title);
+            board.setContent(content);
+            board.setRegDate(new Date(System.currentTimeMillis()));
+            System.out.println("변경하였습니다.");
+            
         } else {
-            board.update();
+            System.out.println("변경을 취소하였습니다.");
         }
     }
     
     private void doDelete() {
         System.out.println("[게시물 삭제]");
-        int no = Integer.parseInt(Prompts.input("번호? "));
+        int no = Prompts.inputInt("번호? ");
         
         Board board = findByNo(no);
         
         if (board == null) {
             System.out.printf("%d번 게시물이 없습니다.\n", no);
+            return;
+        } 
+
+        if (Prompts.confirm2("정말 삭제하시겠습니까?(y/N) ")) {
+            list.remove(board);
+            System.out.println("삭제하였습니다.");
         } else {
-            if (Prompts.confirm2("정말 삭제하시겠습니까?(y/N) ")) {
-                list.remove(board);
-                System.out.println("삭제하였습니다.");
-            } else {
-                System.out.println("삭제를 취소하였습니다.");
-            }
+            System.out.println("삭제를 취소하였습니다.");
         }
     }
     
