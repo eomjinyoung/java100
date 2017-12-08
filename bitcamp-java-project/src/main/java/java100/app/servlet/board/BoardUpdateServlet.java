4eb3832727fe78c8eda7b0a04d24de786f8650fb
eomjinyoung <jinyoung.eom@gmail.com>
2017-12-08 15:30:16 +0900

    .

---
+++ bitcamp-java-project/src/main/java/java100/app/servlet/board/BoardUpdateServlet.java
@@ -0,0 +1,58 @@
+package java100.app.servlet.board;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java100.app.dao.BoardDao;
+import java100.app.domain.Board;
+import java100.app.listener.ContextLoaderListener;
+
+@SuppressWarnings("serial")
+@WebServlet("/board/update")
+public class BoardUpdateServlet extends HttpServlet {
+    
+    @Override
+    public void service(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+
+        BoardDao boardDao = ContextLoaderListener.iocContainer.getBean(
+                BoardDao.class);
+        
+        response.setContentType("text/plain;charset=UTF-8");
+        
+        PrintWriter out = response.getWriter();
+        out.println("[게시물 변경]");
+        
+        try {
+            Board board = new Board();
+            board.setNo(Integer.parseInt(request.getParameter("no")));
+            board.setTitle(request.getParameter("title"));
+            board.setContent(request.getParameter("content"));
+            
+            if (boardDao.update(board) > 0) {
+                out.println("변경하였습니다.");
+            } else {
+                out.printf("'%d'번 게시물이 없습니다.\n", board.getNo());
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace(); // for developer
+            out.println(e.getMessage()); // for user
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
