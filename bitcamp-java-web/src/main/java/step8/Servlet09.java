858f85253b6a87e6b1b3c6623971efaee529ec91
eomjinyoung <jinyoung.eom@gmail.com>
2017-12-12 09:40:51 +0900

    .

--- bitcamp-java-web/src/main/java/step8/Servlet09.java
+++ bitcamp-java-web/src/main/java/step8/Servlet09.java
@@ -5,20 +5,25 @@ package step8;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.oreilly.servlet.MultipartRequest;
+
 @SuppressWarnings("serial")
-@WebServlet("/step8/Servlet08")
+@WebServlet("/step8/Servlet09")
 public class Servlet09 extends HttpServlet {
     
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) 
         throws ServletException, IOException {
         
+        req.setCharacterEncoding("UTF-8");
+
         // 멀티 파트로 전송된 데이터를 분석하여 값과 파일명을 꺼내
         // 다음 변수에 저장할 것이다.
         String name = null;
@@ -27,12 +32,15 @@ public class Servlet09 extends HttpServlet {
         String photo2Filename = null;
         
         // 이 부분에 멀티파트로 전송된 데이터를 분석하는 코드를 작성하라!
+        ServletContext context = this.getServletContext();
+        String path = context.getRealPath("/step8");
+        MultipartRequest multi = new MultipartRequest(req, path, "UTF-8");
+
+        name = multi.getParameter("name");
+        age = multi.getParameter("age");
+        photo1Filename = multi.getFilesystemName("photo1");
+        photo2Filename = multi.getFilesystemName("photo2");
         
-        
-        
-        
-        
-        req.setCharacterEncoding("UTF-8");
         res.setContentType("text/plain;charset=UTF-8");
         PrintWriter out = res.getWriter();
         
