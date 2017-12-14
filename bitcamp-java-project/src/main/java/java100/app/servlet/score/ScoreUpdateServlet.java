9502d0c58199228c700cc1035d136507213fe669
eomjinyoung <jinyoung.eom@gmail.com>
2017-12-14 11:09:51 +0900

    .

--- bitcamp-java-project/src/main/java/java100/app/servlet/score/ScoreUpdateServlet.java
+++ bitcamp-java-project/src/main/java/java100/app/servlet/score/ScoreUpdateServlet.java
@@ -35,10 +35,42 @@ public class ScoreUpdateServlet extends HttpServlet {
         out.println(".container {");
         out.println("    width: 680px;");
         out.println("}");
+        out.println("footer {");
+        out.println("    text-align: center;");
+        out.println("}");
         out.println("</style>");
         out.println("</head>");
         out.println("<body>");
         out.println("<div class='container'>");
+        
+        out.println("<header>");
+        out.println("<nav class='navbar navbar-expand-lg navbar-light bg-light'>");
+        out.println("<a class='navbar-brand' href='../index.html'>");
+        out.println("  <img src='../images/bootstrap-solid.svg' width='30' height='30' class='d-inline-block align-top' alt=''>");
+        out.println("  비트캠프");
+        out.println("</a>");
+        out.println("<button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>");
+        out.println("  <span class='navbar-toggler-icon'></span>");
+        out.println("</button>");
+        out.println("<div class='collapse navbar-collapse' id='navbarNav'>");
+        out.println("<ul class='navbar-nav'>");
+        out.println("  <li class='nav-item'>");
+        out.println("    <a class='nav-link' href='../score/list'>성적</a>");
+        out.println("  </li>");
+        out.println("  <li class='nav-item'>");
+        out.println("    <a class='nav-link' href='../member/list'>회원</a>");
+        out.println("  </li>");
+        out.println("  <li class='nav-item'>");
+        out.println("    <a class='nav-link' href='../board/list'>게시판</a>");
+        out.println("  </li>");
+        out.println("  <li class='nav-item'>");
+        out.println("    <a class='nav-link' href='../room/list'>강의실</a>");
+        out.println("  </li>");
+        out.println("</ul>");
+        out.println("</div>");
+        out.println("</nav>");
+        out.println("</header>");
+        
         out.println("<h1>성적 변경</h1>");
         
         try {
@@ -60,7 +92,16 @@ public class ScoreUpdateServlet extends HttpServlet {
             out.println(e.getMessage()); // for user
         }
         out.println("<p><a href='list' class='btn btn-primary btn-sm'>목록</a></p>");
+        out.println("<footer>");
+        out.println("    비트캠프 자바100기@2017");
+        out.println("</footer>");
+        
         out.println("</div>");
+        
+        out.println("<script src='../node_modules/jquery/dist/jquery.slim.min.js'></script>");
+        out.println("<script src='../node_modules/popper.js/dist/umd/popper.min.js'></script>");
+        out.println("<script src='../node_modules/bootstrap/dist/js/bootstrap.min.js'></script>");
+        
         out.println("</body>");
         out.println("</html>");
     }
