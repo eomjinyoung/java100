f8695d7750276f67cce918e14bba64b8d14b628d
eomjinyoung <jinyoung.eom@gmail.com>
2017-12-28 14:26:35 +0900

    .

---
+++ bitcamp-mybatis/README.md
@@ -0,0 +1,16 @@
+# mybatis 사용법 익히기
+- mybatis는 DBMS와 연동하여 데이터를 등록,조회,수정,삭제하는 작업을 도와준다.
+- 이런 라이브러리를 "persistence API" 또는 "persistence framework"이라 부른다.
+- JDBC 프로그래밍을 작성할 때 중복되는 코드를 별도의 라이브러리로 만든 것이다.
+- 개발자가 직접 JDBC API를 사용하는 대신에 mybatis를 사용하여 데이터의 등록,조회,수정,삭제를 처리한다.
+- 물론 mybatis는 내부적으로 JDBC API를 사용하여 처리한다.
+- 따라서 mybatis를 사용하려면 여전히 JDBC Driver가 있어야 한다.
+
+## 주요 목표
+- 반복적인 JDBC 코딩을 대신 처리해주어 간결한 코드 작성을 도와준다!
+- 자바 소스 코드에서 SQL문을 분리하여 코드의 유지보수를 쉽게 하도록 도와준다!
+
+## step1
+- mybatis 적용 전
+- 직접 JDBC API를 사용하여 데이터 처리
+
---
+++ bitcamp-mybatis/src/main/java/step1/Test2.java
@@ -0,0 +1,43 @@
+// DBMS 서버에게 SQL문을 보내기 - update
+// => 데이터 입출력 테스트를 위한 테이블 생성하라!
+//    bitcamp-docs/jdbcexam.sql 을 실행하라!
+// 
+package step1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Test2 {
+
+    public static void main(String[] args) throws Exception {
+        
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/studydb",
+                "study", "1111");
+        
+        System.out.println("연결되었음!");
+        
+        Statement stmt = con.createStatement();
+        
+        // Statement 객체의 메서드를 통해 SQL문을 서버에 보낸다.
+        stmt.executeUpdate(
+            "update jdbc_test set title='xxx', conts='xxxx' where no=1");
+        
+        System.out.println("변경 완료!");
+        
+        stmt.close();
+        con.close();
+    }
+
+}
+
+
+
+
+
+
+
+
+
