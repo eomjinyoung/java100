d023dae9d2fc61591f16f4ef2dd095625234dadb
Jinyoung Eom <jinyoung.eom@gmail.com>
2018-01-24 17:21:09 +0900

    '.'

--- bitcamp-dbmodel/sql/select04.sql
+++ bitcamp-dbmodel/sql/select04.sql
@@ -1,31 +1,16 @@
-/* as */
+/* union 과 union all */
 
-/* 출력 라벨명을 변경하기
-   => 라벨명을 지정하지 않으면 컬럼명이 라벨명이 된다*/
-select rno as room_no, loc as location, name 
-from room;
+/* select 결과 합치기 
+   union : 중복 값 자동 제거*/
+select distinct bank from stnt
+union
+select distinct bank from tcher;
 
-/* as 생략 가능 */
-select rno room_no, loc location, name 
-from room;
+/* union all: 중복 값 제거 안함*/
+select distinct bank from stnt
+union all
+select distinct bank from tcher;
 
-/* 라벨명에 공백을 넣고 싶으면 '' 안에 작성한다.*/
-select rno 'room no', loc location, name 
-from room;
-
-/* 복잡한 형식으로 출력할 경우 라벨명(별명)을 부여한다. 
-    예) 강의실명(지점명)*/
-select concat(name, '(', loc, ')')
-from room;
-
-select concat(name, '(', loc, ')') title
-from room;
-
-select count(*) 
-from room;
-
-select count(*) cnt
-from room;
 
 
 
