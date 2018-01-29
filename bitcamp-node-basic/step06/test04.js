d1cb2f4259c09c342501c0174f6b1a47a6c109a2
eomjinyoung <jinyoung.eom@gmail.com>
2018-01-29 14:46:25 +0900

    .

---
+++ bitcamp-node-basic/step06/test04.js
@@ -0,0 +1,14 @@
+// test04.js
+
+// 계산 결과를 저장할 변수
+var result = 0;
+
+// result 변수에 값을 더한다.
+function plus(value) {
+    window.result += value;
+}
+
+// result 변수에서 값을 뺀다.
+function minus(value) {
+    window.result -= value;
+}
